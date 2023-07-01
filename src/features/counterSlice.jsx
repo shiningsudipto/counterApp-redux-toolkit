28e4d8ecbb5b4c6b5ba4886b947a03a7f25a5203
Sudipto Das <shiningsudipto@gmail.com>
2023-07-01 07:54:44 +0600

    first commit

---
+++ src/features/counterSlice.jsx
@@ -0,0 +1,19 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+export const counterSlice = createSlice({
+    name: 'counterApp',
+    initialState: { count: 0 },
+    reducers: {
+        increment: (state) => {
+            state.count = state.count + 1;
+        },
+        decrement: (state) => {
+            state.count = state.count - 1;
+        },
+        reset: (state) => {
+            state.count = 0;
+        },
+    }
+})
+export const { increment, decrement, reset } = counterSlice.actions;
+export default counterSlice.reducer;
